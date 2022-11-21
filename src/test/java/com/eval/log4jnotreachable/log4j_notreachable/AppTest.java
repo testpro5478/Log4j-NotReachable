880e123b4fc5f1b9f02ffcf963e63827aa6e2cc6
Sriram Akurati <sakurati@sakurati-mac.attlocal.net>
2022-11-21 12:28:57 -0800

    Log4j NotReachable

---
+++ src/test/java/com/eval/log4jnotreachable/log4j_notreachable/AppTest.java
@@ -0,0 +1,20 @@
+package com.eval.log4jnotreachable.log4j_notreachable;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
